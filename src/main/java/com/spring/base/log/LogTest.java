a8bd0fbc4573cd4c360c14ae6735e18e03317616
quanhz <157957329@qq.com>
2019-09-18 14:11:43 +0800

    1、利用aop加自定义注解在controller层添加日志操作 测试地址http://localhost:9999/sso/log

---
+++ src/main/java/com/spring/base/log/LogTest.java
@@ -0,0 +1,22 @@
+package com.spring.base.log;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ：quanhz
+ * @date ：Created in 2019/9/18 9:24
+ *
+ */
+public class LogTest {
+    private static Map<Integer,BaseOperLog> logs = new HashMap<>();
+    public static void put(BaseOperLog baseOperLog){
+        int key = logs.size()+1;
+        logs.put(key,baseOperLog);
+    }
+
+
+    public static Map<Integer,BaseOperLog> getLogs(){
+        return logs;
+    }
+}
